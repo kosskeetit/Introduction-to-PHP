0e54099ce7887b528c65d1119980c2ea3d3f580e
kosskeetit <leroykipruto57@gmail.com>
2020-02-04 11:42:25 +0300

    introduction to bootsrap

---
+++ lesson1.php
@@ -0,0 +1,36 @@
+<?php
+//Introduction to php
+//PHP is a widely-used, open source scripting language
+//PHP scripts are executed on the server
+//PHP files can contain text, HTML, CSS, Javascript and PHP code
+//php code is executed on the server, and the result is returned to the browser as
+//PHP files have extension ".php"
+echo "hello World";
+echo "<h1> Hello World<h1>";
+//echo: print content in browser
+//hello World; the content to be printed in the browser
+//; : means end of php statement or instructions
+
+//VARIABLES IN PHP
+//a container for storing data
+$myname = "John Doe <br>";
+echo $myname;
+$company1 = "Dell <br>";
+$company2 = "Ubisoft <br>";
+$company3 = "Nutella <br>";
+echo $company1;
+echo $company2;
+echo $company3;
+
+
+
+
+
+
+
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores modi nesciunt,
+    nulla odio sunt tenetur?
+</p>
+
